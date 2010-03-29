a90d274942309342c7eb150a7d016db11f246db8
demoder <demoder@eb168245-8976-4d73-b9b8-9914e7513f76>
2010-03-29 21:56:13 +0000

    Added fancy compile progress reporting Added a basic help system

--- trunk/Demoder.Common/Xml.cs
+++ trunk/Demoder.Common/Xml.cs
@@ -23,7 +23,6 @@ THE SOFTWARE.
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.IO;
 using System.Xml.Serialization;
--- trunk/Demoder.Common/md5.cs
+++ trunk/Demoder.Common/md5.cs
@@ -26,7 +26,6 @@ THE SOFTWARE.
  */
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Security.Cryptography;
 using System.IO;
