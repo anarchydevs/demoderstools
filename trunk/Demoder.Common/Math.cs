6cbc38728742aa65b1f37f09800994eed99e32c6
demoder <demoder@eb168245-8976-4d73-b9b8-9914e7513f76>
2010-03-30 07:15:20 +0000

    Added a class for general math methods

---
+++ trunk/Demoder.Common/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demoder.Common
+{
+	public static class Math
+	{
+		public static int Percent (int max, int cur) {
+			return (int)System.Math.Round((decimal)(cur * 100 / max), 0);
+		}
+	}
+}
