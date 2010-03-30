5e0f48ace4edb244fb50d634a556120998cb8f10
demoder <demoder@eb168245-8976-4d73-b9b8-9914e7513f76>
2010-03-30 08:43:09 +0000

    Added header image file to binfiles Changed AOMC assembly name to AOMC (C#)

--- AOMC/Properties/AssemblyInfo.cs
+++ AOMC/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AOMC")]
+[assembly: AssemblyTitle("AOMC (C#)")]
 [assembly: AssemblyDescription("A map compiler for the game Anarchy Online")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
