299331d9d429aa5eedef7b4a660f5d082cbb9014
demoder <demoder@eb168245-8976-4d73-b9b8-9914e7513f76>
2010-08-04 14:25:34 +0000

    add: forgot to add this file.

---
+++ Demoder.Patcher/Demoders Patcher/backgroundWorker_CreatePatchServer.cs
@@ -0,0 +1,71 @@
+﻿/*
+MIT Licence
+Copyright (c) 2010 Demoder <demoder@flw.nu> (project: https://sourceforge.net/projects/demoderstools/)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Demoders_Patcher.DataClasses;
+using Demoder.Patcher.DataClasses;
+using Demoder.Common;
+
+namespace Demoders_Patcher
+{
+	public class backgroundWorker_CreatePatchServer
+	{
+		private BackgroundWorker _bgw = null;
+
+		public backgroundWorker_CreatePatchServer(BackgroundWorker bgw) {
+			this._bgw = bgw;
+		}
+
+		public void CreatePatchServer(CreatePatchServerConfig CPSC)
+		{
+			PatchServer patchserver = new PatchServer();
+			//Add download locations
+			foreach (string s in CPSC.download_locations)
+				patchserver.download_locations.Add(s);
+			//Name
+			patchserver.Name = CPSC.Name;
+			//Version
+			patchserver.Version = CPSC.Version;			
+			//Set GUID
+			patchserver.GUID = CPSC.GUID;
+
+			//Add distributions
+			foreach (CreateDistributionConfig cdc in CPSC.Distributions)
+			{
+				List<string> dirs = new List<string>();
+				dirs.Add(cdc.Directory);
+				Distribution dist = new Distribution(dirs, cdc.DistributionType);
+				dist.DistributionName = cdc.Name;
+				patchserver.Distributions.Add(dist);
+			}
+			if (this._bgw.CancellationPending)
+				return;
+			//Export.
+			patchserver.Export(CPSC.OutputDirectory);
+			
+		}
+	}
+}
